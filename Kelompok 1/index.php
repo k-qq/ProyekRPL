407c330be9aa1d3e1b902fd3c2b0ce99bbcb465a
mhendrahaqq <muhammadhendra@student.uns.ac.id>
2017-05-11 17:42:39 +0700

    kodingan

---
+++ Kelompok 1/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--    <link rel="stylesheet" href="css/bootsrap.css">-->
+    <link rel="stylesheet" href="css/index.css">
+    <title>Kartika Kusuma Sari Hotel</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="body">
+        <div class="logo container">
+            <img src="img/logo.png">
+        </div>
+
+
+        <ul class="nav-sign container">
+            <li><a href="#">SIGN IN</a></li>
+            <li><a href="#">SIGN OUT</a></li>
+
+        </ul>
+        <ul class="nav-menu container">
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">ROOMS</a></li>
+            <li><a href="#">FACILITIES</a></li>
+            <li><a href="#">PACKAGE</a></li>
+            <li><a href="#">CONTACT US</a></li>
+        </ul>
+        <div id="keunggulan" class="container">
+            <div class="row">
+                <div class="kelebihan col-sm-12">
+                   
+                    <div id="plus" class="container">
+                        <div class="row">
+                            <div class="col-sm-4">                  
+                                
+                            </div>
+                            <div class="col-sm-8">                              
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
